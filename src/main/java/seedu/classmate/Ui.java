67c2560479dfdecb3c36239d742922e91d134835
wn <lauweina@gmail.com>
2026-03-19 22:37:56 +0800

    Add Ui class and basic print commands printLine() and printWelcome()

---
+++ src/main/java/seedu/classmate/Ui.java
@@ -0,0 +1,17 @@
+package seedu.classmate;
+
+public class Ui {
+
+    public void printLine() {
+        System.out.println("_".repeat(30));
+    }
+
+    public void printWelcome() {
+        //port over welcome statement
+    }
+
+    public void printGoodbye() {
+        //code
+    }
+
+}
