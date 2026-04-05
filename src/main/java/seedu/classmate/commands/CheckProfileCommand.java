d6d05b6a3610d8e7bc321b445c9620b903910db0
wn <lauweina@gmail.com>
2026-04-05 17:23:30 +0800

    Update CheckProfile logic (draft) to additionally include the list of incomplete modules

--- src/main/java/seedu/classmate/commands/CheckProfileCommand.java
+++ src/main/java/seedu/classmate/commands/CheckProfileCommand.java
@@ -1,6 +1,7 @@
 package seedu.classmate.commands;
 
 import seedu.classmate.Major;
+import seedu.classmate.Module;
 import seedu.classmate.SpecialisationOverview;
 import seedu.classmate.Ui;
 import seedu.classmate.UserProfile;
@@ -14,6 +15,7 @@ import java.util.logging.Logger;
  */
 public class CheckProfileCommand extends Command {
 
+    // @@author Michael-coding06
     private static final Logger logger = Logger.getLogger(CheckProfileCommand.class.getName());
     private final UserProfile userProfile;
 
@@ -37,25 +39,53 @@ public class CheckProfileCommand extends Command {
     @Override
     public void executeCommand(Major major, Ui ui, SpecialisationOverview specialisationOverview) {
         logger.info("Executing CheckProfileCommand");
+    // @@author
 
-        ArrayList<String> userSpecialisations = userProfile.getUserSpecialisations();
+        // @@author lauwn-mower
+        /*
+         * Review CheckProfile command:
+         * command to additionally show a list of uncompleted modules.
+         * implementation: import CEG module list from major.getCoreModules()
+         *                 create new ArrayList of incomplete modules
+         *                  create a progress tracker via Ui
+         *                  pass in userprofile, remaining major and remaining spec modules
+         *                  for spec elective modules, tell user how many completed
+         *                      and show options as to what else they can do
+         */
+
+        // settle completed major modules first
         ArrayList<String> completedModules = userProfile.getUserCompletedModules();
 
-        System.out.println("Your Academic Profile:");
-        Ui.printLine();
-
-        System.out.println("Specialisation: " + userSpecialisations);
-        
-        System.out.println("Completed Modules:");
-        if (completedModules.isEmpty()) {
-            System.out.println("  - No modules marked as completed yet.");
-        } else {
-            for (String moduleCode : completedModules) {
-                System.out.println("  [DONE] " + moduleCode);
+        // calculate remaining modules
+        ArrayList<Module> remainingMajorCore = new ArrayList<>();
+        for (seedu.classmate.Module m : major.getCoreModules()) {
+            if (!completedModules.contains(m.getModuleCode())) {
+                remainingMajorCore.add(m);
             }
         }
 
-        Ui.printLine();
-        System.out.println("Keep up the great work!");
+        // TODO: delegate to ui to generate basic acad profile
+//        System.out.println("Your Academic Profile:");
+//        Ui.printLine();
+
+        // settle spec modules
+        // first check if spec is empty
+        // else, show specs and modules yet to complete
+        ArrayList<String> userSpecialisations = userProfile.getUserSpecialisations();
+//        System.out.println("Specialisation: " + userSpecialisations);
+//
+//        System.out.println("Completed Modules:");
+//        if (completedModules.isEmpty()) {
+//            System.out.println("  - No modules marked as completed yet.");
+//        } else {
+//            for (String moduleCode : completedModules) {
+//                System.out.println("  [DONE] " + moduleCode);
+//            }
+//        }
+//
+//        Ui.printLine();
+//        System.out.println("Keep up the great work!");
     }
+
+    // @@author
 }
